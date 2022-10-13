0635eec673c98e764a4eb67447ae4339053b8b7b
tobiakinlade <tobi.akinlade@yahoo.com>
2022-10-14 00:16:22 +0100

    authorize permission

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -16,4 +16,10 @@ const authenticateUser = async (req, res, next) => {
   next()
 }
 
-module.exports = { authenticateUser }
+const authorizePermissions = async (req, res, next) => {
+  if (req.user.role !== 'admin') {
+    throw new CustomError.UnauthorizedError('Unauthorized to access this route')
+  }
+  next()
+}
+module.exports = { authenticateUser, authorizePermissions }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { authenticateUser } = require('../middleware/authentication')
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middleware/authentication')
 const {
   getAllUsers,
   getSingleUser,
@@ -9,7 +12,7 @@ const {
   showCurrentUser,
 } = require('../controllers/userController')
 
-router.route('/').get(authenticateUser, getAllUsers)
+router.route('/').get(authenticateUser, authorizePermissions, getAllUsers)
 router.route('/showMe').get(showCurrentUser)
 router.route('/updateUser').patch(updateUser)
 router.route('/updateUserPassword').patch(updateUserPassword)
