f1a66a497fbfbfb2ed53ad96c8999f19060a143e
tobiakinlade <tobi.akinlade@yahoo.com>
2022-10-13 20:54:30 +0100

    get all users and single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,20 @@
+const { StatusCodes } = require('http-status-codes')
 const User = require('../models/User')
+const CustomError = require('../errors')
+const { attachCookiesToResponse } = require('../utils')
 
-const getAllUsers = (req, res) => {
-  res.send('This is an e-commerce website')
+const getAllUsers = async (req, res) => {
+  const users = await User.find({ role: 'user' }).select('-password')
+
+  res.status(StatusCodes.OK).json({ users })
 }
 
-const getSingleUser = (req, res) => {
-  res.send('Get single user')
+const getSingleUser = async (req, res) => {
+  const user = await User.findOne({ _id: req.params.id }).select('-password')
+  if (!user) {
+    throw new CustomError.NotFoundError(` No user with id ${req.params.id}`)
+  }
+  res.status(StatusCodes.OK).json({ user })
 }
 const showCurrentUser = (req, res) => {
   res.send('Show current user')
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,8 +10,8 @@ const {
 
 router.route('/').get(getAllUsers)
 router.route('/showMe').get(showCurrentUser)
-router.route('/updateUser').post(updateUser)
-router.route('/updateUserPassword').post(updateUserPassword)
+router.route('/updateUser').patch(updateUser)
+router.route('/updateUserPassword').patch(updateUserPassword)
 router.route('/:id').get(getSingleUser)
 
 module.exports = router
