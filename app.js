c3cd59ad62e7865982ce377b8fd95c43614f0a35
tobiakinlade <tobi.akinlade@yahoo.com>
2022-10-18 22:43:52 +0100

    security

--- app.js
+++ app.js
@@ -1,60 +1,76 @@
-require('dotenv').config()
-require('express-async-errors')
+require('dotenv').config();
+require('express-async-errors');
 
-require('dotenv').config()
-require('express-async-errors')
+require('dotenv').config();
+require('express-async-errors');
 
-const express = require('express')
-const authRouter = require('./routes/authRoute')
-const userRouter = require('./routes/userRoutes')
-const productRouter = require('./routes/productRoutes')
-const reviewRouter = require('./routes/reviewRoutes')
-const orderRouter = require('./routes/orderRoutes')
-const fileUpload = require('express-fileupload')
-const app = express()
+const express = require('express');
+const authRouter = require('./routes/authRoute');
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const orderRouter = require('./routes/orderRoutes');
+const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+const app = express();
 
-const morgan = require('morgan')
-const cookieParser = require('cookie-parser')
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
-app.use(morgan('tiny'))
-app.use(cookieParser(process.env.JWT_SECRET))
+app.use(morgan('tiny'));
+app.use(cookieParser(process.env.JWT_SECRET));
 
-const connectDB = require('./db/connect')
+const connectDB = require('./db/connect');
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(express.static('./public'))
-app.use(fileUpload())
+app.use(express.static('./public'));
+app.use(fileUpload());
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found')
-const errorHandlerMiddleware = require('./middleware/error-handler')
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.get('/api/v1', (req, res) => {
-  console.log(req.signedCookies)
-  res.send('This is an e-commerce website')
-})
+  console.log(req.signedCookies);
+  res.send('This is an e-commerce website');
+});
 
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/users', userRouter)
-app.use('/api/v1/products', productRouter)
-app.use('/api/v1/reviews', reviewRouter)
-app.use('/api/v1/orders', orderRouter)
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/orders', orderRouter);
 
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 const start = async () => {
-  await connectDB(process.env.MONGO_URI)
+  await connectDB(process.env.MONGO_URI);
   try {
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
-    )
+    );
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-start()
+start();
